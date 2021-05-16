d1636247d27f0e63e70090f936172fdf8b85e717
ALFREDO ASENSIO <alfredoasensio94@gmail.com>
2021-05-16 22:43:24 +0200

    Refactor - Remove unused lines of code

--- src/ubu/gii/dass/refactoring/MovieType.java
+++ src/ubu/gii/dass/refactoring/MovieType.java
@@ -1,9 +1,6 @@
 package ubu.gii.dass.refactoring;
 
 public abstract class MovieType {
-	public static final int CHILDRENS = 2;
-	public static final int REGULAR = 0;
-	public static final int NEW_RELEASE = 1;
 
 	abstract double getCharge(Rental rental);
 
